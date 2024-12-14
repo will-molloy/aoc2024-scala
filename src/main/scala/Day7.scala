9849c1bd8404a76172d78a458047a48a8d5e7b6a
Will Molloy <willjoemolloy@gmail.com>
2024-12-15 02:54:50 +1300

    Day7 Part2 - only example passes
    Not sure what the issue is...

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -3,27 +3,35 @@ package aoc2024
 object Day7 {
 
   def part1(lines: Seq[String]): Long = {
-    lines.map(result).sum
+    lines.map(result(_, Seq('+', '*'))).sum
   }
 
-  private def result(line: String) : Long = {
+  def part2(lines: Seq[String]): Long = {
+    lines.map(result(_, Seq('+', '*', '|'))).sum
+  }
+
+  private def result(line: String, ops: Seq[Char]): Long = {
     val colon = line.indexOf(':')
     val target = line.take(colon).toLong
     val nums = line.drop(colon + 1).split(' ').filterNot(_.isBlank).map(_.toLong)
 
-    def dfs(i: Int, op: Char, result: Long): Long = {
+    def dfs(i: Int, op: Char, result: Long): Boolean = {
       if (result == target) {
-        result
+        true
       } else if (i == nums.length) {
-        0
+        false
       } else {
         op match
-          case '+' => Math.max(dfs(i + 1, '+', result + nums(i)), dfs(i + 1, '*', result + nums(i)))
-          case '*' => Math.max(dfs(i + 1, '+', result * nums(i)), dfs(i + 1, '*', result * nums(i)))
-          case _ => 0
+          case '+' => ops.exists(op => dfs(i + 1, op, result + nums(i)))
+          case '*' => ops.exists(op => dfs(i + 1, op, result * nums(i)))
+          case '|' => ops.exists(op => dfs(i + 1, op, (result.toString + nums(i).toString).toLong))
       }
     }
 
-    Math.max(dfs(1, '+', nums.head), dfs(1, '*', nums.head))
+    if (ops.exists(op => dfs(1, op, nums.head))) {
+      target
+    } else {
+      0
+    }
   }
 }
--- src/test/scala/Day7Test.scala
+++ src/test/scala/Day7Test.scala
@@ -5,11 +5,16 @@ import org.scalatest.funsuite.AnyFunSuiteLike
 import scala.io.Source
 
 class Day7Test extends AnyFunSuiteLike {
-  private val example = false
+  private val example = true
 
   test("part1") {
     val input = readInput((if example then "example_input" else "real_input") + "/day7_input.txt")
     assert(Day7.part1(input) === 3749)
+  }
+
+  test("part2") {
+    val input = readInput((if example then "example_input" else "real_input") + "/day7_input.txt")
+    assert(Day7.part2(input) === 11387)
   }
 
   private def readInput(fileName: String): Seq[String] = {
