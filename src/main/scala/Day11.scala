a649252e9d5e31445c9636c99767a152ac33a5c5
Will Molloy <willjoemolloy@gmail.com>
2024-12-22 01:02:40 +1300

    Day11 part2 - DP
    I guess the key is understanding the order doesn't matter and each stone
    is independant. I.e. they will be broken up separately, then sum at the
    end.

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -8,39 +8,31 @@ import scala.collection.mutable
  * https://adventofcode.com/2024/day/11
  */
 object Day11 extends Day[Seq[Long], Long](2024, 11) {
+  private val dp = mutable.HashMap[(Long, Int), Long]()
 
   override def part1(input: Seq[Long]): Long = {
-    var stones = input.toBuffer
-    for (_ <- 1 to 25) {
-      stones = blink(stones)
-    }
-    stones.size
-  }
-
-  private def blink(line: mutable.Buffer[Long]): mutable.Buffer[Long] = {
-    val buffer = mutable.ArrayBuffer[Long]()
-    for (e <- line) {
-      if (e == 0) {
-        buffer.addOne(1)
-      } else {
-        val digits = e.toString
-        if (digits.length % 2 == 0) {
-          val (front, back) = digits.splitAt(digits.length / 2)
-          buffer.addOne(front.toLong)
-          buffer.addOne(back.toLong)
-        } else {
-          buffer.addOne(e * 2024)
-        }
-      }
-    }
-    buffer
+    input.map(amount(_, 25)).sum
   }
 
   override def part2(input: Seq[Long]): Long = {
-    var stones = input.toBuffer
-    for (_ <- 1 to 75) {
-      stones = blink(stones)
-    }
-    stones.size
+    input.map(amount(_, 75)).sum
+  }
+
+  private def amount(stone: Long, blinks: Int): Long = {
+    dp.getOrElseUpdate((stone, blinks), {
+      if (blinks == 0) {
+        1
+      } else if (stone == 0) {
+        amount(1, blinks - 1)
+      } else {
+        val digits = stone.toString
+        if (digits.length % 2 == 0) {
+          val (front, back) = digits.splitAt(digits.length / 2)
+          amount(front.toLong, blinks - 1) + amount(back.toLong, blinks - 1)
+        } else {
+          amount(stone * 2024, blinks - 1)
+        }
+      }
+    })
   }
 }
--- src/test/scala/Day11Test.scala
+++ src/test/scala/Day11Test.scala
@@ -11,5 +11,5 @@ class Day11Test extends DayTest(Day11) {
 
   override def part1 = Expected(55312, 239714)
 
-  override def part2 = Expected(31, 20373490)
+  override def part2 = Expected(65601038650482L, 284973560658514L)
 }
